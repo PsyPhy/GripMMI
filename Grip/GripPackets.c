d193a9b8c71489150614868e0f77c44fdc2d24aa
frenchjam <joe.mcintyre@parisdescartes.fr>
2014-09-28 22:29:24 +0200

    Updates to Grip library.

--- Grip/GripPackets.c
+++ Grip/GripPackets.c
@@ -2,7 +2,11 @@
 // Packet definitions for realtime data from Grip.
 //
 
+#include "../Useful/Useful.h"
 #include "GripPackets.h"
+
+// Routines to change the byte order in various data types.
+// These are useful when inserting or extracting data from an EPM packet.
 
 unsigned short swapbytes_short( unsigned short input ) {
 	union {
@@ -28,7 +32,21 @@ unsigned long swapbytes_long( unsigned long input ) {
 	return( out.value );
 }
 
+// These routines are used to insert or extract various data types 
+//  from a byte buffer without changing the byte order.
 unsigned  short extract_short( const unsigned char bytes[2] ) {
+	union {
+		unsigned short value;
+		unsigned char  byte[2];
+	} out;
+	out.byte[0] = bytes[0];
+	out.byte[1] = bytes[1];
+	return( out.value );
+}
+
+// These routines are used to insert or extract various data types 
+//  in reverse byte order from a byte buffer.
+unsigned  short extract_reversed_short( const unsigned char bytes[2] ) {
 	union {
 		unsigned short value;
 		unsigned char  byte[2];
@@ -38,7 +56,7 @@ unsigned  short extract_short( const unsigned char bytes[2] ) {
 	return( out.value );
 }
 
-unsigned long extract_long( const unsigned char bytes[4] ) {
+unsigned long extract_reversed_long( const unsigned char bytes[4] ) {
 	union {
 		unsigned long value;
 		unsigned char  byte[4];
@@ -50,72 +68,184 @@ unsigned long extract_long( const unsigned char bytes[4] ) {
 	return( out.value );
 }
 
+float extract_reversed_float( const unsigned char bytes[4] ) {
+	union {
+		float			value;
+		unsigned char	byte[4];
+	} out;
+	out.byte[0] = bytes[3];
+	out.byte[1] = bytes[2];
+	out.byte[2] = bytes[1];
+	out.byte[3] = bytes[0];
+	return( out.value );
+}
+// These macros facilate stepping through a buffer with a pointer and make the code cleaner.
+// On each use of the macro, the pointer is incremented by the number of bytes corresponding to the data type.
+// Use of the macros reduces the chance of error due to mismatch between data type and increment of the pointer.
+// Note the comma, rather than semicolon, between statements. This make it OK to put it in a single-line 
+// 'if' or 'for' statement, i.e. "for ( i = X; i <= Z; i++ ) position[i] = ExtractShort( ptr );"
+#define ExtractShort( ptr ) extract_short( ptr ), ptr += sizeof( short ) 
+#define ExtractReversedShort( ptr ) extract_reversed_short( ptr ), ptr += sizeof( short ) 
+#define ExtractReversedLong( ptr ) extract_reversed_long( ptr ), ptr += sizeof( long ) 
+#define ExtractReversedFloat( ptr ) extract_reversed_float( ptr ), ptr += sizeof( float ) 
+#define ExtractChar( ptr ) (*ptr++)
+
 /***********************************************************************************/
 
-void InsertEPMTelemetryHeaderInfo ( EPMTelemetryPacket *epm_packet, const EPMTelemetryHeaderInfo *header  ) {
+// Fill a structure with the header values from an EPM Transfer Frame packet.
+// The bytes are in ESA/EPM order in the packet, and need to be reversed for Windows.
+// Returns the number of bytes inserted into the buffer.
+int InsertEPMTransferFrameHeaderInfo ( EPMTelemetryPacket *epm_packet, const EPMTransferFrameHeaderInfo *header  ) {
+	unsigned char *ptr = ((unsigned char *) epm_packet); 
+	unsigned int  bytes_inserted = 0;
+	*((long *)ptr)= swapbytes_long( header->epmLanSyncMarker ); ptr += sizeof( unsigned long ); bytes_inserted += sizeof( unsigned long);
+	*ptr =  header->spare1; ptr++; bytes_inserted++;
+	*ptr = header->softwareUnitID; ptr++; bytes_inserted++;
+	*((unsigned short *)ptr) = swapbytes_short( header->packetType ); ptr += sizeof( unsigned short ); bytes_inserted += sizeof( unsigned short);
+	*((unsigned short *)ptr) = swapbytes_short( header->spare2 ); ptr += sizeof( unsigned short );  bytes_inserted += sizeof( unsigned short);
+	*((unsigned short *)ptr) = swapbytes_short( header->numberOfWords ); ptr += sizeof( unsigned short );  bytes_inserted += sizeof( unsigned short);
+	return( bytes_inserted );
+}
 
+void ExtractEPMTransferFrameHeaderInfo ( EPMTransferFrameHeaderInfo *header, const EPMTelemetryPacket *epm_packet  ) {
+	unsigned char *ptr = ((unsigned char *) epm_packet); 
+	header->epmLanSyncMarker = ExtractReversedLong( ptr );
+	header->spare1 = ExtractChar( ptr );
+	header->softwareUnitID = ExtractChar( ptr );
+	header->packetType = ExtractReversedShort( ptr );
+	header->spare2 = ExtractReversedShort( ptr );
+	header->numberOfWords = ExtractReversedShort( ptr );
+}
+
+// Insert Telemetry header info into a buffer in byte-reversed order.
+// Returns the number of bytes inserted.
+int InsertEPMTelemetryHeaderInfo ( EPMTelemetryPacket *epm_packet, const EPMTelemetryHeaderInfo *header  ) {
 	// Fill a structure with the header values from an EPM TCP packet.
 	// The bytes are in ESA/EPM order in the TCP packet, and need to be reversed for Windows.
 	unsigned char *ptr = ((unsigned char *) epm_packet); 
-	*((long *)ptr)= swapbytes_long( header->epmSyncMarker ); ptr += sizeof( unsigned long );
-	*ptr = header->subsystemMode; ptr++;
-	*ptr = header->subsystemID; ptr++;
-	*ptr = header->destination; ptr++;
-	*ptr = header->subsystemUnitID; ptr++;
-	*((unsigned short *)ptr) = swapbytes_short( header->TMIdentifier ); ptr += sizeof( short );
-	*((unsigned short *)ptr) = swapbytes_short( header->TMCounter ); ptr += sizeof( short );
-	*ptr = header->model; ptr++;
-	*ptr = header->taskID; ptr++;
-	*((unsigned short *)ptr) = swapbytes_short( header->subsystemUnitVersion ); ptr += sizeof( short );
-	*((unsigned long  *)ptr) = swapbytes_long( header->coarseTime ); ptr += sizeof( long );
-	*((unsigned short *)ptr) = swapbytes_short( header->fineTime ); ptr += sizeof( short );
-	*ptr = header->timerStatus; ptr++;
-	*ptr = header->experimentMode; ptr++;
-	*((unsigned short *)ptr) = swapbytes_short( header->checksumIndicator ); ptr += sizeof( short );
-	*ptr = header->receiverSubsystemID; ptr++;
-	*ptr = header->receiverSubsystemUnitID; ptr++;
-	*((unsigned short *)ptr) = swapbytes_short( header->numberOfWords ); ptr += sizeof( short );
+	unsigned int  bytes_inserted = 0;
+
+	bytes_inserted = InsertEPMTransferFrameHeaderInfo ( epm_packet, &header->transferFrameInfo );
+	ptr += bytes_inserted;
+
+	*((long *)ptr)= swapbytes_long( header->epmSyncMarker ); ptr += sizeof( unsigned long ); bytes_inserted += sizeof( unsigned long );
+	*ptr = header->subsystemMode; ptr++; bytes_inserted++;
+	*ptr = header->subsystemID; ptr++; bytes_inserted++;
+	*ptr = header->destination; ptr++; bytes_inserted++;
+	*ptr = header->subsystemUnitID; ptr++; bytes_inserted++;
+	*((unsigned short *)ptr) = swapbytes_short( header->TMIdentifier ); ptr += sizeof( unsigned short );  bytes_inserted += sizeof( unsigned short );
+	*((unsigned short *)ptr) = swapbytes_short( header->TMCounter ); ptr += sizeof( unsigned short ); bytes_inserted += sizeof( unsigned short );
+	*ptr = header->model; ptr++; bytes_inserted++;
+	*ptr = header->taskID; ptr++; bytes_inserted++;
+	*((unsigned short *)ptr) = swapbytes_short( header->subsystemUnitVersion ); ptr += sizeof( unsigned short ); bytes_inserted += sizeof( unsigned short );
+	*((unsigned long  *)ptr) = swapbytes_long( header->coarseTime ); ptr += sizeof( unsigned long ); bytes_inserted += sizeof( unsigned long );
+	*((unsigned short *)ptr) = swapbytes_short( header->fineTime ); ptr += sizeof( unsigned short ); bytes_inserted += sizeof( unsigned short );
+	*ptr = header->timerStatus; ptr++; bytes_inserted++;
+	*ptr = header->experimentMode; ptr++; bytes_inserted++;
+	*((unsigned short *)ptr) = swapbytes_short( header->checksumIndicator ); ptr += sizeof( unsigned short ); bytes_inserted += sizeof( unsigned short );
+	*ptr = header->receiverSubsystemID; ptr++; bytes_inserted++;
+	*ptr = header->receiverSubsystemUnitID; ptr++; bytes_inserted++;
+	*((unsigned short *)ptr) = swapbytes_short( header->numberOfWords ); ptr += sizeof( unsigned short ); bytes_inserted += sizeof( unsigned short );
+	return( bytes_inserted );
 }
 
 void ExtractEPMTelemetryHeaderInfo ( EPMTelemetryHeaderInfo *header, const EPMTelemetryPacket *epm_packet  ) {
-
 	// Fill a structure with the header values from an EPM TCP packet.
 	// The bytes are in ESA/EPM order in the TCP packet, and need to be reversed for Windows.
 	unsigned char *ptr = ((unsigned char *) epm_packet); 
-	header->epmSyncMarker = extract_long( ptr ); ptr += sizeof( unsigned long );
-	header->subsystemMode = *ptr; ptr++;
-	header->subsystemID = *ptr; ptr++;
-	header->destination= *ptr; ptr++;
-	header->subsystemUnitID = *ptr; ptr++;
-	header->TMIdentifier = extract_short( ptr ); ptr += sizeof( short );
-	header->TMCounter = extract_short( ptr ); ptr += sizeof( short );
-	header->model = *ptr; ptr++;
-	header->taskID = *ptr; ptr++;
-	header->subsystemUnitVersion = extract_short( ptr ); ptr += sizeof( short );
-	header->coarseTime = extract_long( ptr ); ptr += sizeof( long );
-	header->fineTime = extract_short( ptr ); ptr += sizeof( short );
-	header->timerStatus = *ptr; ptr++;
-	header->experimentMode = *ptr; ptr++;
-	header->checksumIndicator = extract_short( ptr ); ptr += sizeof( short );
-	header->receiverSubsystemID = *ptr; ptr++;
-	header->receiverSubsystemUnitID = *ptr; ptr++;
-	header->numberOfWords = extract_short( ptr ); ptr += sizeof( short );
 
+	header->transferFrameInfo.epmLanSyncMarker = ExtractReversedLong( ptr );
+	header->transferFrameInfo.spare1 = ExtractChar( ptr );
+	header->transferFrameInfo.softwareUnitID = ExtractChar( ptr );
+	header->transferFrameInfo.packetType = ExtractReversedShort( ptr );
+	header->transferFrameInfo.spare2 = ExtractReversedShort( ptr );
+	header->transferFrameInfo.numberOfWords = ExtractReversedShort( ptr );
+
+	header->epmSyncMarker = ExtractReversedLong( ptr );
+	header->subsystemMode = ExtractChar( ptr );
+	header->subsystemID = ExtractChar( ptr );
+	header->destination= ExtractChar( ptr );
+	header->subsystemUnitID = ExtractChar( ptr );
+	header->TMIdentifier = ExtractReversedShort( ptr );
+	header->TMCounter = ExtractReversedShort( ptr );
+	header->model = ExtractChar( ptr );
+	header->taskID = ExtractChar( ptr );
+	header->subsystemUnitVersion = ExtractReversedShort( ptr );
+	header->coarseTime = ExtractReversedLong( ptr );
+	header->fineTime = ExtractReversedShort( ptr );
+	header->timerStatus = ExtractChar( ptr );
+	header->experimentMode = *ptr; ptr++;
+	header->checksumIndicator = ExtractReversedShort( ptr );
+	header->receiverSubsystemID = ExtractChar( ptr );
+	header->receiverSubsystemUnitID = ExtractChar( ptr );
+	header->numberOfWords = ExtractReversedShort( ptr ); 
 }
 
-// Extract a real-time data packet from an EPM packet.
-void ExtractGripRealtimeDataPacket( GripRealtimeDataPacket *realtime_packet, const EPMTelemetryPacket *epm_packet ) {
-	unsigned char *ptr = ((unsigned char *) epm_packet);
+// Extract a real-time science data packet from an EPM packet.
+void ExtractGripRealtimeDataInfo( GripRealtimeDataInfo *realtime_packet, const EPMTelemetryPacket *epm_packet ) {
+	const char *ptr;
 	int slice;
-	// Skip over the EPM header.
-	ptr += 30;
+	int i;
+	// Point to the actual data in the packet.
+	ptr = epm_packet->sections.rawData;
 	// Get the acquisition ID and packet count for that acquisition.
-	realtime_packet->acquisitionID = extract_long( ptr ); ptr += sizeof( unsigned long );
-	realtime_packet->rtPacketCount = extract_long( ptr ); ptr += sizeof( unsigned long );
+	realtime_packet->acquisitionID = ExtractReversedLong( ptr );
+	realtime_packet->rtPacketCount = ExtractReversedLong( ptr );
 	for ( slice = 0; slice < RT_SLICES_PER_PACKET; slice++ ) {
 		// Get the manipulandum pose data. 
-		// For the moment, just get the ticks and jump to the next entry.
-		realtime_packet->dataSlice[slice].poseTick = extract_long( ptr ); ptr += 35;
-		realtime_packet->dataSlice[slice].analogTick = extract_long( ptr ); ptr += 40;
+		realtime_packet->dataSlice[slice].poseTick = ExtractReversedLong( ptr );
+		for ( i = X; i <= Z; i++ ) realtime_packet->dataSlice[slice].position[i] = ExtractReversedShort( ptr );
+		for ( i = X; i <= M; i++ ) realtime_packet->dataSlice[slice].quaternion[i] = ExtractReversedFloat( ptr );
+		for ( i = 0; i < 2; i++ ) realtime_packet->dataSlice[slice].markerVisibility[i] = ExtractReversedLong( ptr );
+		realtime_packet->dataSlice[slice].manipulandumVisibility = ExtractChar( ptr );
+		// Get the analog data.
+		realtime_packet->dataSlice[slice].analogTick = ExtractReversedLong( ptr );
+		// For now we skip over the analog values.
+		ptr += 36;
 	}
+}
+
+// Extract a Grip housekeeping packet from an EPM packet.
+// Careful! It is not complete. Not all values are filled.
+void ExtractGripHealthAndStatusInfo( GripHealthAndStatusInfo *health_packet, const EPMTelemetryPacket *epm_packet ) {
+
+	const char *ptr;
+	int slice;
+	int i;
+
+	// Point to the actual data in the packet.
+	ptr = epm_packet->sections.rawData;
+
+	// Skip to the script information.
+	ptr += 76;
+
+	health_packet->horizontalTargetFeedback = ExtractReversedShort( ptr );
+	health_packet->verticalTargetFeedback = ExtractReversedShort( ptr );
+
+	health_packet->toneFeedback = ExtractChar( ptr );
+	health_packet->cradleDetectors = ExtractChar( ptr );
+
+	health_packet->user = ExtractReversedShort( ptr );
+	health_packet->protocol = ExtractReversedShort( ptr );
+	health_packet->task = ExtractReversedShort( ptr );
+	health_packet->step = ExtractReversedShort( ptr );
+
+	health_packet->scriptEngineStatusEnum = ExtractReversedShort( ptr );
+	health_packet->iochannelStatusEnum = ExtractReversedShort( ptr );
+	health_packet->motionTrackerStatusEnum = ExtractReversedShort( ptr );
+	health_packet->crewCameraStatusEnum = ExtractReversedShort( ptr );
+
+	health_packet->crewCameraRate = ExtractReversedShort( ptr );
+
+	health_packet->runningBits = ExtractReversedShort( ptr );
+	health_packet->cpuUsage = ExtractReversedShort( ptr );
+	health_packet->memoryUsage = ExtractReversedShort( ptr );
+
+	health_packet->freeDiskSpaceC = ExtractReversedLong( ptr );
+	health_packet->freeDiskSpaceD = ExtractReversedLong( ptr );
+	health_packet->freeDiskSpaceE = ExtractReversedLong( ptr );
+
+	health_packet->crc = ExtractReversedShort( ptr );
+	
+
 }
--- Grip/GripPackets.h
+++ Grip/GripPackets.h
@@ -10,15 +10,47 @@
 #define EPM_DEFAULT_PORT "2345"
 
 // Per EPM-OHB-SP-0005, packets shall not exceed 1412 octets.
-#define EPM_BUFFER_LENGTH	1412	
+#define EPM_BUFFER_LENGTH	1412
+#define EPM_TRANSFER_FRAME_HEADER_LENGTH	12
+#define EPM_TELEMETRY_HEADER_LENGTH			30
 
-// Telemetry packets have the following sync marker.
-#define EPM_TELEMETRY_SYNC_VALUE	0xFFDB544D
+// Definitions for Transfer Frame headers.
+#define EPM_TRANSFER_FRAME_SYNC_VALUE	0xAA49DBFF
+#define TRANSFER_FRAME_CONNECT			0x0001
+#define TRANSFER_FRAME_ALIVE			0x0002
+#define TRANSFER_FRAME_TELECOMMAND		0x1154
+#define TRANSFER_FRAME_TELEMETRY		0x1153
+
+#define GRIP_MMI_SOFTWARE_UNIT_ID		0x43
+#define GRIP_SUBSYSTEM_ID				0x21
+
+// These constants help make it clear in initialization lists
+//  when we are just filling a spare slot in a structure or
+//  when we are initializing a field for which the value is not
+//  yet known and will be filled in later.
+#define SPARE	0
+#define UNKNOWN	0
+
+// Definitions for Telemetry Packet headers.
+#define EPM_TELEMETRY_SYNC_VALUE		0xFFDB544D
 #define GRIP_HK_ID	0x0301
 #define GRIP_RT_ID	0x1001
 
+// Compute the time in seconds.
 #define EPMtoSeconds( header ) ((long double)header.coarseTime + ((long double) header.fineTime / 10000.0))
+
 typedef struct {
+	unsigned long	epmLanSyncMarker;
+	unsigned char	spare1;
+	unsigned char	softwareUnitID;
+	unsigned short	packetType;
+	unsigned short	spare2;
+	unsigned short	numberOfWords;
+} EPMTransferFrameHeaderInfo;
+
+typedef struct {
+
+	EPMTransferFrameHeaderInfo	transferFrameInfo;
 
 	unsigned long  epmSyncMarker;
 	unsigned char  subsystemMode;
@@ -46,8 +78,8 @@ typedef struct {
 	unsigned long	poseTick;
 	short			position[3];
 	float			quaternion[4];
-	unsigned long	markerVisbility[2];  // One for each coda;
-	unsigned char	visibility;
+	unsigned long	markerVisibility[2];  // One for each coda;
+	unsigned char	manipulandumVisibility;
 	// DATA_IOC_FTG
 	unsigned long	analogTick;
 	short			ft[2][6];
@@ -59,35 +91,116 @@ typedef struct {
 	unsigned long acquisitionID;
 	unsigned long rtPacketCount;
 	ManipulandumPacket dataSlice[RT_SLICES_PER_PACKET];
-} GripRealtimeDataPacket;
+} GripRealtimeDataInfo;
+
+typedef struct {
+
+#if 0
+
+	// ExtractGripHealthAndStatusInfo() does not yet fill in the entire structure.
+	// These values are hidden inside the #if 0 so that no one tries to use them.
+	// When ExtractGripHealthAndStatusInfo() gets update, these items can be exposed again.
+	unsigned short	currentMode;
+	unsigned short	nextMode;
+	unsigned short	timerStatus;
+	unsigned short	correctiveAction;
+	unsigned short	fileTransferStatus;
+	// Various temperature and voltage readings.
+	// We don't use them, so I don't name them separately.
+	short	temperature[10];
+	short	voltage[8];
+	unsigned short	selftest;
+	float	rxDataRate;
+	float	txDataRate;
+	unsigned short	fanStatus;
+	unsigned short	epmInteraceStatusEnum;
+	
+	unsigned long	unexplained;
+
+	unsigned short	smokeDetectorStatus;
+	unsigned short  OCD;
+
+#endif
+
+	unsigned short	horizontalTargetFeedback;
+	unsigned short	verticalTargetFeedback;
+	unsigned char	toneFeedback;
+	unsigned char	cradleDetectors;
+
+	unsigned short	user;
+	unsigned short	protocol;
+	unsigned short	task;
+	unsigned short	step;
+
+	unsigned short	scriptEngineStatusEnum;
+	unsigned short	iochannelStatusEnum;
+	unsigned short	motionTrackerStatusEnum;
+	unsigned short	crewCameraStatusEnum;
+
+	unsigned short	crewCameraRate; // fps
+
+	unsigned short	runningBits;	// Bit 0: shell command  Bit 1: system acquiring
+	unsigned short	cpuUsage;		// percent
+	unsigned short	memoryUsage;	// percent
+
+	unsigned long	freeDiskSpaceC;
+	unsigned long	freeDiskSpaceD;
+	unsigned long	freeDiskSpaceE;
+
+	unsigned short	crc;
+
+} GripHealthAndStatusInfo;
 
 typedef union {
 	// A buffer containing the entire packet.
-	char					buffer[EPM_BUFFER_LENGTH];
+	char buffer[EPM_BUFFER_LENGTH];
 	// Allows easy acces to the header elements by name.
-	EPMTelemetryHeaderInfo	byte_reversed_header;
-} EPMTelemetryPacket;
+	struct {
+		char rawTransferFrameHeader[EPM_TRANSFER_FRAME_HEADER_LENGTH];
+		char rawTelemetryHeader[EPM_TELEMETRY_HEADER_LENGTH];
+		char rawData[EPM_BUFFER_LENGTH - (EPM_TRANSFER_FRAME_HEADER_LENGTH + EPM_TELEMETRY_HEADER_LENGTH)];
+	} sections;
+} EPMTelemetryPacket; 
+
+// Define a static lan packet for sending a connect command from the GRIP-MMI to EPM.
+static EPMTransferFrameHeaderInfo connectPacket = { EPM_TRANSFER_FRAME_SYNC_VALUE, SPARE, GRIP_MMI_SOFTWARE_UNIT_ID, TRANSFER_FRAME_CONNECT, SPARE, 6 };
+static int connectPacketLengthInBytes = 12;
+static int connectPacketLengthInWords = 6;
+
+static EPMTransferFrameHeaderInfo alivePacket = { EPM_TRANSFER_FRAME_SYNC_VALUE, SPARE, GRIP_MMI_SOFTWARE_UNIT_ID, TRANSFER_FRAME_ALIVE, SPARE, 6 };
+static int alivePacketLengthInBytes = 12;
+static int alivePacketLengthInWords = 6;
 
 // Define a static packet header that is representative of a housekeeping packet.
 // We don't try to simulate all the details, so most of the parameters are set to zero.
 // The TM Identifier is 0x0301 for DATA_BULK_HK per DEX-ICD-00383-QS.
-// The total number of words is 114 / 2 = 57 for the GRIP packet, 15 for the EPM header and 1 for the checksum.
-static EPMTelemetryHeaderInfo hkHeader = { EPM_TELEMETRY_SYNC_VALUE, 0, 0, 0, 0, GRIP_HK_ID, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-static int hkPacketLengthInBytes = 146;
+// The total number of words is 114 / 2 = 57 for the GRIP packet, 6 for the Transfer Frame header, 
+//  15 for the Telemetry header and 1 for the checksum.
+static EPMTelemetryHeaderInfo hkHeader = { 
+	EPM_TRANSFER_FRAME_SYNC_VALUE, SPARE, GRIP_MMI_SOFTWARE_UNIT_ID, TRANSFER_FRAME_TELEMETRY, SPARE, 158,
+	EPM_TELEMETRY_SYNC_VALUE, 0, GRIP_SUBSYSTEM_ID, 0, 0, GRIP_HK_ID, UNKNOWN, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+static int hkPacketLengthInBytes = 158;
 
-// Do the same for a realtime data packet.
+// Define a static packet header that is representative of a realtime data packet.
+// Not all of the members are properly filled. Just the ones important for the GripMMMI.
 // The TM Identifier is 0x1001 for DATA_RT_SCIENCE per DEX-ICD-00383-QS.
-// The total number of words is 758 / 2 = 379 for the GRIP packet, 15 for the EPM header and 1 for the checksum.
-static EPMTelemetryHeaderInfo rtHeader = { EPM_TELEMETRY_SYNC_VALUE, 0, 0, 0, 0, GRIP_RT_ID, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-static int rtPacketLengthInBytes = 790;
+// The total number of words is 758 / 2 = 379 for the GRIP packet, 6 for the Transfer Frame header,
+//  15 for the EPM header and 1 for the checksum.
+static EPMTelemetryHeaderInfo rtHeader = { 
+	EPM_TRANSFER_FRAME_SYNC_VALUE, SPARE, GRIP_MMI_SOFTWARE_UNIT_ID, TRANSFER_FRAME_TELEMETRY, SPARE, 796,
+	EPM_TELEMETRY_SYNC_VALUE, 0, GRIP_SUBSYSTEM_ID, 0, 0, GRIP_RT_ID, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+static int rtPacketLengthInBytes = 796;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+int  InsertEPMTransferFrameHeaderInfo ( EPMTelemetryPacket *epm_packet, const EPMTransferFrameHeaderInfo *header  );
+void ExtractEPMTransferFrameHeaderInfo ( EPMTransferFrameHeaderInfo *header, const EPMTelemetryPacket *epm_packet );
 void ExtractEPMTelemetryHeaderInfo ( EPMTelemetryHeaderInfo *header, const EPMTelemetryPacket *epm_packet  );
-void InsertEPMTelemetryHeaderInfo ( EPMTelemetryPacket *epm_packet,  const EPMTelemetryHeaderInfo *header  );
-void ExtractGripRealtimeDataPacket( GripRealtimeDataPacket *realtime_packet, const EPMTelemetryPacket *epm_packet );
+int  InsertEPMTelemetryHeaderInfo ( EPMTelemetryPacket *epm_packet,  const EPMTelemetryHeaderInfo *header  );
+void ExtractGripRealtimeDataInfo( GripRealtimeDataInfo *realtime_packet, const EPMTelemetryPacket *epm_packet );
+void ExtractGripHealthAndStatusInfo( GripHealthAndStatusInfo *health_packet, const EPMTelemetryPacket *epm_packet );
 
 #ifdef __cplusplus
 }
