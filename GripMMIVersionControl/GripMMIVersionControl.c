f4fdacd648683a51d669d7cc8b1a7d2a0655f48a
frenchjam <joe.mcintyre@parisdescartes.fr>
2015-02-10 17:05:11 +0100

    Release Version 1.1.1

--- GripMMIVersionControl/GripMMIVersionControl.c
+++ GripMMIVersionControl/GripMMIVersionControl.c
@@ -15,9 +15,9 @@
 /// Edit the lines below prior to compiling a new release.
 
 #ifdef _DEBUG 
-char *GripMMIVersion = "GripMMI V1.1.0 (debug)";
+char *GripMMIVersion = "GripMMI V1.1.1 (debug)";
 char *GripMMIBuildInfo = "Build "__DATE__" "__TIME__;
 #else
-char *GripMMIVersion = "GripMMI V1.1.0";
+char *GripMMIVersion = "GripMMI V1.1.1";
 char *GripMMIBuildInfo = "Build "__DATE__" "__TIME__;
 #endif
