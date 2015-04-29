f5286a1e0dd1d35676c880ea4ed9027e19f760ae
frenchjam <psyphy@comcast.net>
2015-04-29 10:21:43 -0700

    Issue #16 Documentation of Emulator
    RunGripMMI.bat has been modified to make it easier to configure
    CLWSemulator.exe according to the new functionality. The comments have
    been improved to show the emulator options.
    
    The Installation instructions have also been modified to better
    describe the emulator test options.
    
    NB: The “Theory of Operation” chapter of the on-line wiki has also been
    updated to describe options for testing with the CLWS emulator.

--- CreateStandaloneGripMMI/RunGripMMI.bat
+++ CreateStandaloneGripMMI/RunGripMMI.bat
@@ -11,28 +11,60 @@ REM **************************************************************************
 
 REM Edit the definitions in this section according to the installation.
 
-REM Comment out the next line to disable the CLWS emulator.
+REM
+REM CLWS Emulator for testing
+REM 
+
+REM The batch file starts up, by default, a CLWS emulator for local testing.
+REM For final installation, comment out the next line to disable the CLWS emulator.
 set EMULATE=TRUE
 
-REM Directory holding cache files
-set CacheDir=..\GripCache\
+REM The next lines determine the mode of the CLWS emulator.
+REM It is currently configured to send out pre-recorded packets.
+REM set EMULATE_MODE=-constructed
+set EMULATE_MODE=-recorded
+
+REM You can substitute a different file as the source of the recorded packets
+REM  by changing the following definition. Note that this definition has
+REM  no effect if you are in "-constructed" mode.
+set PACKET_SOURCE=".\\GripPacketsForSimulator.gpk"
 
 REM Host Name or Address of the CLWS Server.
 REM Can be 'localhost' if running on same machine.
-REM Should be 'localhost' if running the CLWSemulator (see below).
+REM Should be 'localhost' if running the CLWSemulator (see above).
 set HOST=localhost
 
+REM
+REM Directory holding cache files
+REM
+
+REM The client program will store packets received from the CLWS in this directory.
+set CacheDir=..\GripCache\
+
+REM
 REM Directory holding the Grip scripts (.dex)
+REM
 set ScriptDir=..\GripScripts\
 
-REM Uncomment the next line to use the alternate Software Unit ID.
+REM
+REM Alternate Software Unit ID.
+REM
+REM The CLWS can accept only one connection from a given Software Unit.
+REM If you want to use two client connected to the same CLWS server,
+REM  uncomment the next line to use the alternate Software Unit ID.
 REM set UNIT=-alt
 
-REM Uncomment the next line if you want to display times in UTC rather than GPS
+REM 
+REM UTC versus GPS time of day.
+REM
+
+REM EPM used GPS time-of-day, but CADMOS displays UTC time. There is a small shift between them,
+REM  because UTC takes into account leap seconds, while GPS does not.
 REM Default value is -16 which corrects for leap seconds as of Jan. 2015.
 REM Modify the value if new leap seconds have been added (e.g. after July 2015 change to -17).
-set TIMEBASE_CORRECTION=-16
 
+REM Uncomment the next line if you want to display times in UTC rather than GPS
+set TIMEBASE_CORRECTION=-16
 REM Uncomment the next line if you want to display in GPS time.
 REM set TIMEBASE_CORRECTION=0
 
@@ -46,10 +78,11 @@ set CacheRoot=GripPackets.%date:~10,4%.%date:~4,2%.%date:~7,2%
 
 echo %cd%
 
-REM Start up the emulator if you want to test without the actual GRIP
-REM  or EPM hardware. Comment out or delete for real operations.
+REM Starts up the emulator according to configuration defined above.
 if not defined EMULATE goto LAUNCH
-start .\CLWSemulator.exe
+REM CLWSemulator takes a mode flag, -constructed or -recorded, and
+REM  an optional path to the pre-recorded EPM packets.
+start .\CLWSemulator.exe %EMULATE_MODE% %PACKET_SOURCE%
 
 :LAUNCH
 REM Launch the TCP/IP Client.
