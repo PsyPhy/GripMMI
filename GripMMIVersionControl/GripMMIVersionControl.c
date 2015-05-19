e2fb4171e367d5a5323c609bd82685c45c5eb90c
frenchjam <psyphy@comcast.net>
2015-05-19 17:41:15 +0200

    Prepare for a new minor release (1.3.0).
    Some changes since the last release are not simply bug fixes. There are
    enough functional changes to merit a bump of the minor release number.

--- GripMMIVersionControl/GripMMIVersionControl.c
+++ GripMMIVersionControl/GripMMIVersionControl.c
@@ -15,9 +15,9 @@
 /// Edit the lines below prior to compiling a new release.
 
 #ifdef _DEBUG 
-char *GripMMIVersion = "GripMMI V1.2.1 (debug)";
+char *GripMMIVersion = "GripMMI V1.3.0 (debug)";
 char *GripMMIBuildInfo = "Build "__DATE__" "__TIME__;
 #else
-char *GripMMIVersion = "GripMMI V1.2.1 (beta)";
+char *GripMMIVersion = "GripMMI V1.3.0 (beta)";
 char *GripMMIBuildInfo = "Build "__DATE__" "__TIME__;
 #endif
