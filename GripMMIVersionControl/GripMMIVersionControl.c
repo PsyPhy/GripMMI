e5265618b5afc881a772515fc2904b504c2130fe
frenchjam <psyphy@comcast.net>
2016-11-11 22:15:30 +0100

    Versions 1.4.0

--- GripMMIVersionControl/GripMMIVersionControl.c
+++ GripMMIVersionControl/GripMMIVersionControl.c
@@ -15,9 +15,9 @@
 /// Edit the lines below prior to compiling a new release.
 
 #ifdef _DEBUG 
-char *GripMMIVersion = "GripMMI V1.3.0 (debug)";
+char *GripMMIVersion = "GripMMI V1.4.0 (debug)";
 char *GripMMIBuildInfo = "Build "__DATE__" "__TIME__;
 #else
-char *GripMMIVersion = "GripMMI V1.3.0";
+char *GripMMIVersion = "GripMMI V1.4.0";
 char *GripMMIBuildInfo = "Build "__DATE__" "__TIME__;
 #endif
